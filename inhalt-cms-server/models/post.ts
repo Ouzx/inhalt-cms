c8b9dbc452b2b83897cdeecc816ed8e80fd721b7
Ouzx <oguzhankandakoglu@gmail.com>
2023-01-12 02:57:22 +0300

    post model doc

--- inhalt-cms-server/models/post.ts
+++ inhalt-cms-server/models/post.ts
@@ -1,14 +1,38 @@
+/**
+ * @description Post model
+ * @version 1.0.0
+ * @author Ouzx
+ * @date 1/11/2023 - 11:30:33 PM
+ * @module models/post
+ *
+ * @requires mongoose : MongoDB ORM
+ * @requires models/user : User model
+ *
+ * @exports iPost : Post interface
+ * @exports default : Post model
+ */
 import mongoose, { Document, Schema, model } from "mongoose";
 import User from "./user.js";
 
 /**
- * Description placeholder
- * @date 1/11/2023 - 11:13:07 PM
- *
- * @export
+ * @description Post interface
+ * @version 1.0.0
+ * @author Ouzx
+ * @date 1/11/2023 - 11:30:33 PM
  * @interface iPost
- * @typedef {iPost}
- * @extends {Document}
+ *
+ * @property {string} title - Post title
+ * @property {string} content - Post content
+ * @property {string} category - Post category
+ * @property {string[]} tags - Post tags
+ * @property {string} thumbnail - Post thumbnail
+ * @property {string} raw - Post raw content
+ * @property {boolean} shared - Post shared
+ * @property {string} urlSuffix - Post url suffix
+ * @property {string} shortContent - Post short content
+ * @property {Date} createdAt - Post created at
+ * @property {Date} updatedAt - Post updated at
+ * @property {typeof User} user - Post user
  */
 export interface iPost extends Document {
   title: string;
@@ -25,6 +49,16 @@ export interface iPost extends Document {
   user: typeof User;
 }
 
+/**
+ * @description Post schema
+ * @version 1.0.0
+ * @author Ouzx
+ * @date 1/11/2023 - 11:30:33 PM
+ *
+ * @const
+ * @type {Schema<iPost>}
+ * @implements {iPost}
+ */
 const postSchema = new Schema<iPost>({
   title: {
     type: String,
@@ -80,6 +114,7 @@ const postSchema = new Schema<iPost>({
   },
 });
 
+// Create post model
 const Post = model("Post", postSchema);
 
 export default Post;
