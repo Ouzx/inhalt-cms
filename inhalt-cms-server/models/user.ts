fd398d8ecdd766db3f4cc6396d24bded861b2ac7
Ouzx <oguzhankandakoglu@gmail.com>
2023-01-12 03:00:55 +0300

    user model doc

--- inhalt-cms-server/models/user.ts
+++ inhalt-cms-server/models/user.ts
@@ -1,5 +1,32 @@
+/**
+ * @description User schema
+ * @version 1.0.0
+ * @author Ouzx
+ * @date 1/11/2023 - 11:30:33 PM
+ * @module models/user
+ *
+ * @requires mongoose : Schema, model for typescript type checking
+ *
+ * @exports iUser : User interface
+ * @exports default : User model
+ */
 import { Document, Schema, model } from "mongoose";
 
+/**
+ * @description User interface
+ * @version 1.0.0
+ * @author Ouzx
+ * @date 1/11/2023 - 11:30:33 PM
+ * @interface iUser
+ *
+ * @property {string} username - User username
+ * @property {string} name - User name
+ * @property {string} surname - User surname
+ * @property {string} email - User email
+ * @property {string} password - User password
+ * @property {string} image - User image
+ * @property {Date} createdAt - User created date
+ */
 export interface iUser extends Document {
   username: string;
   name: string;
@@ -10,6 +37,16 @@ export interface iUser extends Document {
   createdAt: Date;
 }
 
+/**
+ * @description User schema
+ * @version 1.0.0
+ * @author Ouzx
+ * @date 1/11/2023 - 11:30:33 PM
+ *
+ * @const
+ * @type {Schema<iUser>}
+ * @implements {iUser}
+ */
 const userSchema = new Schema<iUser>(
   {
     username: {
@@ -66,6 +103,7 @@ const userSchema = new Schema<iUser>(
   }
 );
 
+// Create user model
 const User = model("User", userSchema);
 
 export default User;
