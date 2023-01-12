29ac4f9830222a12c6e62afc290e051743e4906f
Ouzx <oguzhankandakoglu@gmail.com>
2023-01-12 03:07:40 +0300

    image route doc

--- inhalt-cms-server/routes/image.ts
+++ inhalt-cms-server/routes/image.ts
@@ -1,11 +1,27 @@
+/**
+ * @description Image routes
+ * @version 1.0.0
+ * @author Ouzx
+ * @date 1/11/2023 - 11:30:33 PM
+ * @module routes/image
+ *
+ * @requires express
+ * @requires controllers/image
+ * @requires middlewares/imager
+ */
 import { Router } from "express";
 import { multerMiddlewareSingle } from "../middlewares/imager.js";
 
 import { byFile, byURL } from "../controllers/image.js";
 
+// Create router
 const router = Router();
-
+// First upload image by file later save file url to database
 router.post("/by-file", multerMiddlewareSingle, byFile);
+
+// TODO: TEST THIS!
+// Upload image by url
 router.post("/by-url", byURL);
 
+// Export router
 export default router;
