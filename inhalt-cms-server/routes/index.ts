02aac9d851b8da032cdd0d41eca91e07f3d3b592
Ouzx <oguzhankandakoglu@gmail.com>
2023-01-12 03:10:52 +0300

    routes index doc

--- inhalt-cms-server/routes/index.ts
+++ inhalt-cms-server/routes/index.ts
@@ -1,3 +1,14 @@
+/**
+ * @description This file exports all the routes in the routes folder
+ * @version 1.0.0
+ * @author Ouzx
+ * @date 1/11/2023 - 11:30:33 PM
+ * @module routes/index
+ *
+ * @requires routes/auth
+ * @requires routes/image
+ * @requires routes/post
+ */
 import auth from "./auth.js";
 import image from "./image.js";
 import post from "./post.js";
