53cae443f13d7b30dbb8e2aa7eda00a462b642f4
Ouzx <oguzhankandakoglu@gmail.com>
2023-01-12 03:04:11 +0300

    auth routes doc

--- inhalt-cms-server/routes/auth.ts
+++ inhalt-cms-server/routes/auth.ts
@@ -1,11 +1,30 @@
+/**
+ * @description Auth routes
+ * @version 1.0.0
+ * @author Ouzx
+ * @date 1/11/2023 - 11:30:33 PM
+ * @module routes/auth
+ *
+ * @requires express
+ * @requires controllers/auth
+ * @requires middlewares/imager
+ */
 import { Router } from "express";
 import { register, login, validate } from "../controllers/auth.js";
 import { multerMiddlewareSingle } from "../middlewares/imager.js";
 
+// Create router
 const router = Router();
 
-// router.post("/register", multerMiddlewareSingle, register); // REGISTER DISABLED
+// Register disabled for preventing multiple users
+// uncomment when its needed
+// router.post("/register", multerMiddlewareSingle, register);
+
+// Login
 router.post("/login", login);
+
+// Validate token
 router.get("/validate-token", validate);
 
+// export it to use as app.use()
 export default router;
